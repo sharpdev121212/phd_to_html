3a1b55028ed7eadb80dd4ae58c7bb14bc613922d
iamjohn <john@gmail.com>
2017-07-17 00:29:45 +0300

    Finished everything without animations

--- my.js
+++ my.js
@@ -9,14 +9,76 @@ $('#slider-carousel').carouFredSel({
       duration:500,
       pauseOnHover:true
    },
-   auto:true,
+   auto:false,
    items: {
       visible:{
          min:1,
          max:1
       },
       height:"variable"
+   },
+   pagination: {
+      container: ".sliderpager",
+      anchorBuilder: false,
    }
 
+
+});
+
+
+$('.portofolio-carousel').carouFredSel({
+   responsive:true,
+   width:'100%',
+   circular:true,
+   prev:'#prev',
+   next:'#next',
+   scroll: {
+      items:1,
+      duration:500,
+      pauseOnHover:true
+   },
+   auto:true,
+   items: {
+      visible:{
+         min:1,
+         max:4
+      },
+      height:"variable"
+   }
+
+});
+
+$('.testimonial-carousel').carouFredSel({
+   responsive:true,
+   width:'100%',
+   circular:true,
+   scroll: {
+      items:1,
+      duration:500,
+      pauseOnHover:true
+   },
+   auto:false,
+   items: {
+      visible:{
+         min:1,
+         max:1
+      },
+      height:"variable"
+   },
+   // pagination: {
+   //    container: ".sliderpager",
+   //    anchorBuilder: false,
+   // }
+
+
+});
+
+$(window).scroll(function(){
+   let top= $(window).scrollTop();
+   if(top>120){
+      $('header').css('background-color','#34495e');
+      return;
+   }
+   $('header').css('background-color','unset');
 
 });
